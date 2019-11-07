7570a8bc199439bc4a5e2bbe07a2539f4803cdee
宗帅 <zshuai1722@163.com>
2019-11-07 09:55:41 +0800

    Add files via upload

---
+++ src/main/java/com/zshuai/elasticsearch/ElasticSearch.java
@@ -0,0 +1,11 @@
+package com.zshuai.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearch {
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearch.class);
+    }
+}
---
+++ src/main/java/com/zshuai/elasticsearch/pojo/Item.java
@@ -0,0 +1,35 @@
+package com.zshuai.elasticsearch.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(indexName = "item", type = "docs", shards = 1)
+public class Item {
+
+    @Field(type = FieldType.Long)
+    @Id
+    private Long id;
+
+    @Field(type = FieldType.Text, analyzer = "ik_smart")
+    private String title;
+
+    @Field(type = FieldType.Keyword)
+    private String category;
+
+    @Field(type = FieldType.Keyword)
+    private String brand;
+
+    @Field(type = FieldType.Double)
+    private Double price;
+
+    @Field(type = FieldType.Keyword, index = false)
+    private String images;
+}
